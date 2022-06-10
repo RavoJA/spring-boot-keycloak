d38772961c85ffc30abb56918996cf97bf3b4913
ravoja <jeanaime.ravomanana@gmail.com>
2022-06-10 18:47:50 +0300

    update config keycloak

--- web-app/src/main/java/jean/aime/webapp/KeycloackSecurityConfig.java
+++ web-app/src/main/java/jean/aime/webapp/KeycloackSecurityConfig.java
@@ -2,39 +2,50 @@ package jean.aime.webapp;
 
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@Configuration
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+@Import(KeycloakSpringBootConfigResolver.class)
 public class KeycloackSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
     @Bean
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+        return new RegisterSessionAuthenticationStrategy(buildSessionRegistry());
     }
 
     @Bean
-    public KeycloakConfigResolver keycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) {
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-        auth.authenticationProvider(keycloakAuthenticationProvider);
+    protected SessionRegistry buildSessionRegistry() {
+        return new SessionRegistryImpl();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-
-        http.authorizeRequests().antMatchers("/beers").authenticated();
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
--- web-app/src/main/java/jean/aime/webapp/MainController.java
+++ web-app/src/main/java/jean/aime/webapp/MainController.java
@@ -1,9 +1,17 @@
 package jean.aime.webapp;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.annotation.security.RolesAllowed;
+
 @Controller
+@Slf4j
 public class MainController {
 
     @GetMapping("/")
@@ -11,8 +19,13 @@ public class MainController {
         return "index";
     }
 
+    @RolesAllowed("ROLE_app-admin")
     @GetMapping("/beers")
-    public String beers() {
+    public String beers(Authentication authentication) {
+        log.info("user {}", authentication.getAuthorities());
+        log.info("user details {}", authentication.getDetails());
+        log.info("user principale {}", authentication.getPrincipal());
+        log.info("user name {}", authentication.getName());
         return "beers";
     }
 
